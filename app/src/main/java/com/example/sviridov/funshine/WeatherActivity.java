73e4fb3f041de6bb45f5565f7d58abc12a8aee71
Sviridov <SvyrydovArtem@gmail.com>
2017-03-25 19:22:45 +0200

    initial

---
+++ app/src/main/java/com/example/sviridov/funshine/WeatherActivity.java
@@ -0,0 +1,125 @@
+package com.example.sviridov.funshine;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import org.json.JSONObject;
+
+
+public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
+
+    final String URL_BASE = "http://api.openweathermap.org/data/2.5/forecast";
+    final String URl_COORDS = "/?lat=";//"/?lat=40.739542&lon=-73.991319";
+    final String URL_API_KEY = "&APPID=b47469d9b74fb1ac6ccb6a84294d54ed";
+
+    final int PERMITION_LOCATION = 11111;
+
+    //40.739542, -73.991319
+
+    private GoogleApiClient mGoogleApiClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+    public void downloadWeatherData(Location location) {
+        final String fullCoords = URl_COORDS +location.getLatitude()+"&lon"+location.getLongitude();
+        final String url = URL_BASE + fullCoords + URL_API_KEY;
+
+        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.v("FUN", "RES: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("FUN", "Err: " + error.getLocalizedMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(jsonRequest);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        downloadWeatherData(location);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMITION_LOCATION);
+        } else {
+            startLocationServices();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void startLocationServices() {
+        Log.v("DONKEY", "Starting location Services");
+
+        try {
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, req, this);
+        } catch (SecurityException exception) {
+            //show dialog whi everything is so bad
+            Log.v("DONKEY", "Starting location Services");
+        }
+
+
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMITION_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationServices();
+                } else {
+                    Toast.makeText(this, "Muahahah", Toast.LENGTH_LONG);
+                }
+            }
+        }
+    }
+}
